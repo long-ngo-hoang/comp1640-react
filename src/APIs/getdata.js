801ee4f56d0bb4e76114f1ab9ab19a9227f44ef2
quangvu31 <quangvu31>
2023-02-10 18:55:41 +0700

    use-axios-library

---
+++ src/APIs/getdata.js
@@ -0,0 +1,17 @@
+// import axios from "axios";
+// import React from "react";
+
+// export default function GetData(){
+//     const [data,setdata] =React.useState('');
+
+//         axios.get(`https://localhost:44393/weatherforecast`)
+//         .then((res) =>  {
+//             console.log(res);
+//             // setdata(res.data.data)
+//         })
+//         return (
+//             <div> abc </div>
+
+//         )
+//       }
+
---
+++ src/components/pages/newspage.js
@@ -0,0 +1,5 @@
+export default function NewsPage(){
+    return(
+        <h1>NewsPage</h1>
+    )
+}
