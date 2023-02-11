7998b778aa590ae1ed969b8abe58a79f493012a2
HoanNV081001 <hoannv081001@gmail.com>
2023-02-11 18:27:32 +0700

    Learn React

--- src/components/pages/homepage.js
+++ src/components/pages/homepage.js
@@ -1,5 +1,14 @@
+import GetData  from "../../APIs/getdata"
+import TodoList from "../../APIs/deleteIdea"
+
+
 export default function HomePage(){
     return(
+        <>
         <h1>HomePage</h1>
+        <GetData />
+        <TodoList/>
+        
+        </>
     )
 }
--- src/components/pages/newspage.js
+++ src/components/pages/newspage.js
@@ -1,5 +1,10 @@
+import GetData  from "../../APIs/getdata"
+
 export default function NewsPage(){
     return(
+        <>
         <h1>NewsPage</h1>
+        <GetData />
+        </>
     )
 }
